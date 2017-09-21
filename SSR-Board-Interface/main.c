ad762ed58ceafa8e2ea13be5e69a1ce6c1ae7cc9
Eddie Hurtig <eddie@hurtigtechnologies.com>
2017-09-21 00:58:54 -0400

    Initial Commit

---
+++ SSR-Board-Interface/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SSR-Board-Interface
+//
+//  Created by Eddie Hurtig on 9/21/17.
+//  Copyright © 2017 Eddie Hurtig. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
